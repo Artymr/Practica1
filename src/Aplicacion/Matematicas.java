3e544ecf868a681c9fa7061e4767f6c46e357960
Arturo Muñoz <arturo.munozrodriguez@usp.ceu.com>
2022-02-15 09:55:25 +0100

    Avance casi terminado

--- src/Aplicacion/Matematicas.java
+++ src/Aplicacion/Matematicas.java
@@ -2,6 +2,28 @@ package mates;
 
 public class Matematicas{
 	public static double generarNumeroPiIterativo(long pasos){
-		return 0;
+		int i = 0;
+		int count = 0;
+		double pi = 0;
+		double x = 0;
+		double y = 0;
+		while(i < n) {
+			x = Math.random();
+			y = Math.random();
+			if ((x * x) + (y * y) < 1) {
+				count++;
+			}
+			i++;
+		}
+		pi = 4 * (count / n);
+		return pi;
+	}
+	
+	public static void main(String[] args) {
+		Matematicas problema = new Matematicas();
+		double pi = 0;
+		pi = problema.countPI(300000);
+
+		System.out.println ("El valor es: ");
 	}
 }
